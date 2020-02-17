488c6fe0d4a60fa977771eeb5f252d360bafd181
sibelly <sibellychan@hotmail.com>
2020-02-17 09:58:00 -0400

    Changes the template for vue-cli 4 and adds the components and events for the quotes

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,42 @@
+<template>
+    <form action="">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 
+                        col-md-6 col-md-offset-3 form-group"
+            >
+                <label for="newQuote">Quote</label>
+                <textarea name="newQuote" rows="3"
+                          class="form-control"
+                          v-model="quote"
+                >
+
+                </textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 
+                        col-md-6 col-md-offset-3 form-group"
+            >
+                <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            quote: ''
+        }
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteAdded', this.quote)
+            this.quote = ''
+        }
+    },
+}
+</script>
+
+<style lang="css">
+    
+</style>
